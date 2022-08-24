09e98643534ce4ab33fb83c8c58fccee48550087
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2022-08-24 20:38:31 +0200

    Pagination Done

--- app/src/main/java/com/example/sampleprojectayoub/repository/MainRepository.kt
+++ app/src/main/java/com/example/sampleprojectayoub/repository/MainRepository.kt
@@ -1,9 +1,10 @@
 package com.example.sampleprojectayoub.repository
 
 import com.example.sampleprojectayoub.RetrofitService
-import retrofit2.http.Query
 
 class MainRepository constructor(private val retrofitService: RetrofitService) {
 
-    fun getAllUsers (@Query("q") username : String) = retrofitService.getAllUsers(username)
+    fun getAllUsers (username : String) = retrofitService.getAllUsers(username)
+
+    fun getMoreUsers(username: String, pagenumber : Int) = retrofitService.getMoreUsers(username, pagenumber)
 }
